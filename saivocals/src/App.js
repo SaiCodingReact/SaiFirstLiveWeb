ef184a52b83a1838913497429fa3d3a9c728b3cd
SaiVocals <74341670+SaiCodingReact@users.noreply.github.com>
2020-11-14 13:10:12 +0530

    Updated New Rows

--- saivocals/src/App.js
+++ saivocals/src/App.js
@@ -15,6 +15,12 @@ function App() {
      
      <Row title='Trending Now' fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
      <Row title='Top Rated' fetchUrl={requests.fetchTrending} />
+    <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+    <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+    <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+    <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+    <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+    
     </div>
   );
 }
