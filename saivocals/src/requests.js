4730dab7ebaa03b7ab81f1a24cb99f6c24b21c6b
SaiVocals <74341670+SaiCodingReact@users.noreply.github.com>
2020-11-14 12:58:54 +0530

    Update requests.js

--- saivocals/src/requests.js
+++ saivocals/src/requests.js
@@ -12,7 +12,7 @@ const requests = {
     fetchComedyMovies:`/discover/movie?api_key=${API_Key}&with_genres=35`,
     fetchHorrorMovies:`/discover/movie?api_key=${API_Key}&with_genres=27`,
     fetchRomanceMovies:`/discover/movie?api_key=${API_Key}&with_genres=10749`,
-    fetchDocumentaries`/discover/movie?api_key=${API_Key}&with_genres=99`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_Key}&with_genres=99`,
 }
 
 export default requests;
